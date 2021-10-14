d1321966e4b3a2769e5302a0680fc2ded8fa0b08
Yuyi <yuyi365@gmail.com>
2021-10-14 17:21:29 -0400

    commit tree 6ce961ac4e5d5494671b5fa512e04e3acc01772e

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const splitUp = sentence.split(' ');
+    // this stores the split up array (by words, and no longer a sentence, into a new array) using map
+
+    const capitalizeWords = splitUp.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    //returns a new array using map called "capitalizeWords" that capitalizes the first character of each word in the new array but adds the slice to retain the remainder of the word
+
+    const combineCapitalizedWords = capitalizeWords.join(' ');
+
+    // joins the words back together
+
+    return combineCapitalizedWords;
+
+  });
 }
